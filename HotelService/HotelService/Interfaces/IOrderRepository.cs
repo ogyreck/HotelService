d9443dbe98fb80794a0387d9935565a0a1241007
ogyreck <andrej.peredvigin@mail.ru>
2023-08-04 09:26:53 +0400

    Create-Interface

---
+++ HotelService/HotelService/Interfaces/IOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using HotelService.Models;
+
+namespace HotelService.Interfaces
+{
+    public interface IOrderRepository
+    {
+        Task CreateAsync(Order order);
+        Task<List<Order>> GetAsync(int id);
+        Task UpdateAsync(Order order);
+        Task DeleteAsync(Order order);
+    }
+}
---
+++ HotelService/HotelService/Repositories/OrderRepository.cs
@@ -0,0 +1,43 @@
+﻿using HotelService.Data;
+using HotelService.Interfaces;
+using HotelService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelService.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly ApplicationDbContext _context;
+        
+        public OrderRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task CreateAsync(Order order)
+        {
+            _context.Orders.Add(order);
+            await _context.SaveChangesAsync();
+        }
+        public async Task<List<Order>> GetAsync(int id)
+        {
+
+            return await _context.Orders
+                .OrderByDescending(o => o.RegistraionTime)
+                .ToListAsync(); 
+        }
+
+        public async Task UpdateAsync(Order order)
+        {
+              _context.Orders.Update(order);
+            await _context.SaveChangesAsync();
+        }
+
+        public  async Task DeleteAsync(Order order)
+        {
+            _context.Orders.Remove(order);
+            await _context.SaveChangesAsync();
+        }
+
+        
+    }
+}
